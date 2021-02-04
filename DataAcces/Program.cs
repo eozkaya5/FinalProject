091c4892040224e8882a4e72212dece27a6e6729
eozkaya5 <eozkaya675@gmail.com>
2021-02-05 02:24:45 +0300

    InMemory alt tabanlı dal oluşturuldu.

---
+++ DataAcces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataAcces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
