bb3f42e7f67cdbbb2d8c93f29c7d7792c17e4024
eozkaya5 <eozkaya675@gmail.com>
2021-02-07 03:37:29 +0300

    ICategoryService implement edilerek methodlara iş eklendi.

---
+++ Business/ConCrete/CategoryManager.cs
@@ -0,0 +1,34 @@
+﻿
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ConCrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş kodları
+            return _categoryDal.GetAll();
+        }
+
+        //Select * from Categories where categoryId=3
+        public List<Category> GetById(int categoryId)
+        {
+            return _categoryDal.GetAll(c => c.CategoryId == categoryId);
+        }
+    }
+}
