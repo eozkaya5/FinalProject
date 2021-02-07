83e2debbe72923cf0b196c153735afdfec53185f
eozkaya5 <eozkaya675@gmail.com>
2021-02-07 03:21:01 +0300

    DataAccess'deki IEntitiyRepository Core' a taşındı.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+// Core katmanı diğer katmanları referans almaz
+namespace Core.DataAccess
+{
+    //Generic constraint :generic kısıt
+    //class:Referans tip
+    // IEntity: IEntity veya IEntity implement eden bir nesne olabilir
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class, IEntity,new() 
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+
+        T Get(Expression<Func<T, bool>> filter);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+}
