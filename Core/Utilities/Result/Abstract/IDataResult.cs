9922cd48bd23cd677fb06868a76c6efd9aae908b
eozkaya5 <eozkaya675@gmail.com>
2021-02-11 00:24:27 +0300

    Verileri dahil edebilmek için; "IDataResult" interface oluşturuldu. Oluşturulan interface "Result" sınıfına implement edildi.

---
+++ Core/Utilities/Result/Abstract/IDataResult.cs
@@ -0,0 +1,12 @@
+﻿using Core.Utilities.Result.Absract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result.Abstract
+{
+   public interface IDataResult<T>:IResult
+    {
+        T Data { get; }
+    }
+}
