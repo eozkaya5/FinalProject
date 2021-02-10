31bcb9bb658bde14ec8f1997cb10b46b5b6c8abf
eozkaya5 <eozkaya675@gmail.com>
2021-02-11 00:35:43 +0300

    İşlemlerde kullanılacak mesajlar eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+   public static class Messages //static verdiğinde new' lemene gerek kalmaz
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+
+        public static string ProductNameInvalid = "Ürün ismi geçersizdir.";
+
+        public static string MaintenanceTime="Sistem bakımdadır";
+
+        public static string ProductListed="Ürünler Listelendi.";
+    }
+}
