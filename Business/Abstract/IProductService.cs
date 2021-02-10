806be0b09defb4cdc9ed5ec7201e6afd0450860e
eozkaya5 <eozkaya675@gmail.com>
2021-02-11 00:37:57 +0300

    "IDataResult" interface eklendi. "void"  silinerek "IResult" interface kullanıldı.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,7 @@
 ﻿
+using Core.Utilities.Result;
+using Core.Utilities.Result.Absract;
+using Core.Utilities.Result.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
 using System;
@@ -9,12 +12,16 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
+        IDataResult<List<Product>> GetAll();
 
-        List<Product> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
 
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
 
-        List<ProductDetailDto> GetProductDetailDtos();
+        IDataResult<List<ProductDetailDto>> GetProductDetailDtos();
+
+        IDataResult<List<Product>> GetById(int productId);
+
+        IResult Add(Product product);
     }
 }
