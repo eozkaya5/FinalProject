a41319b8c91d60d9b53a7bc0ea595a26f139fb6d
eozkaya5 <eozkaya675@gmail.com>
2021-02-28 15:07:09 +0300

    Erişim anahtarı oluşturuldu.

---
+++ Core/Utilities/Security/Jwt/AccessToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    //Anlamsızz karakterlerden oluşan erişim anahtarıdır.
+    public class AccessToken //Erişim anahtarı
+    {
+        public string Token { get; set; }
+        public DateTime Expiration  { get; set; } //Bitiş tarihi
+    }
+}
---
+++ Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreteToken(User user, List<OperationClaim> operationClaims);
+        // Kullanıcı bilgileri girildikten sonra eğer doğru ise CreateToken çalışacak.
+        //İlgili kullanıcı için veritabanına gidecek Claimlerini bulacak,claimlerini buluşturacak JWT üretecek Api'ye verecek.
+    }
+}
