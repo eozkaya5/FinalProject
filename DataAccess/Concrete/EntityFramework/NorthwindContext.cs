7d549d827dd4542085c17bd78699a4faa5d591ac
eozkaya5 <eozkaya675@gmail.com>
2021-03-03 19:17:02 +0300

    DbSet eklendi.

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using Core.Entities.Concrete;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -15,11 +16,11 @@ namespace DataAccess.Concrete.EntityFramework
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
         }
         public DbSet<Product> Products  { get; set; }
-
         public DbSet<Category> Categories { get; set; }
-
         public DbSet<Customer> Customers { get; set; }
-
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
